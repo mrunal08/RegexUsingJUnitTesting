df972b1e0d45d2f4457f6bcd332f1303e0943642
mrunal08 <mrunalg96@gmail.com>
2021-09-08 19:41:37 +0530

    UC5-Password Validation

--- main/java/com/regextest/UserValidation.java
+++ main/java/com/regextest/UserValidation.java
@@ -5,5 +5,11 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
 
+    public boolean validateFirstName(String password){
 
+        String passswordRegex= "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z*.!@$%^&(){}:;<>,.?/~_+-=|]).{8,32}$";
+        Pattern pattern = Pattern.compile(passswordRegex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 }
