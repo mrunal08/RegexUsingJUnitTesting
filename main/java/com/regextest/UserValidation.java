fbc56a6d5f35c2b9adab1e754ec8cd3443bee9a2
mrunal08 <mrunalg96@gmail.com>
2021-09-08 17:55:51 +0530

    UC1-FirstName with Min 3 Characters

--- main/java/com/regextest/UserValidation.java
+++ main/java/com/regextest/UserValidation.java
@@ -4,6 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidation {
+    public boolean validateFirstName(String firstName){
 
+        String uc1Regex= "^[A-Z]{1}[a-zA-Z]{2,20}";
+        Pattern pattern = Pattern.compile(uc1Regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
 
 }
