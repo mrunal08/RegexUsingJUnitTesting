f670bd16022c038dcd0c3b29a5432fdc5a51099c
mrunal08 <mrunalg96@gmail.com>
2021-09-08 18:22:18 +0530

    UC2-LastName with Min 3 Characters

--- main/java/com/regextest/UserValidation.java
+++ main/java/com/regextest/UserValidation.java
@@ -5,5 +5,11 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
 
+    public boolean validateLastName(String lastName){
 
+        String UC2Regex= "^[A-Z]{1}[a-zA-Z]{2,20}";
+        Pattern pattern = Pattern.compile(UC2Regex);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
 }
--- test/java/com/regextest/UserValidationTest.java
+++ test/java/com/regextest/UserValidationTest.java
@@ -3,6 +3,45 @@ package com.regextest;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserValidationTest {
 
+    private UserValidation userValidation = new UserValidation();
+
+    @Test
+    public void whenGivenLastNameWith1stCapitalLetter_shouldReturnTrue(){
+
+        boolean validationStatus = userValidation.validateLastName("Gadhave");
+        Assert.assertEquals(true,validationStatus);
+    }
+
+    @Test
+    public void whenGivenLastNameWith1stLetterNotCapital_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateLastName("gadhave");
+        Assert.assertEquals(false,validationStatus);
+    }
+
+    @Test
+    public void whenGivenLastNameContainNumericValue_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateLastName("Gadhave08");
+        Assert.assertEquals(false,validationStatus);
+    }
+
+    @Test
+    public void whenGivenLastNameContainSpecialCharacter_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateLastName("Gadhave@08");
+        Assert.assertEquals(false,validationStatus);
+    }
+
+    @Test
+    public void whenGivenLastNameIsLessThan3Characters_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateLastName("Ga");
+        Assert.assertEquals(false,validationStatus);
+    }
 }
