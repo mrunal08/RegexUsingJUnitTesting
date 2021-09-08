2fb0468fe08219450a1371f75e79ce1ca3af4bec
mrunal08 <mrunalg96@gmail.com>
2021-09-08 18:57:43 +0530

    UC3-PhoneNumber With CountryCode Validation

--- main/java/com/regextest/UserValidation.java
+++ main/java/com/regextest/UserValidation.java
@@ -6,4 +6,13 @@ import java.util.regex.Pattern;
 public class UserValidation {
 
 
+    public boolean validateContactNumber(String contactNumber){
+
+        String uc1Regex= "^[0-9]{1}[0-9]{1}\\s{0,1}[7-9]{1}[0-9]{9}";
+        Pattern pattern = Pattern.compile(uc1Regex);
+        Matcher matcher = pattern.matcher(contactNumber);
+        return matcher.matches();
+    }
+
+
 }
--- test/java/com/regextest/UserValidationTest.java
+++ test/java/com/regextest/UserValidationTest.java
@@ -5,4 +5,39 @@ import org.junit.Test;
 
 public class UserValidationTest {
 
+    private UserValidation userValidation = new UserValidation();
+
+    @Test
+    public void whenGivenContactWithCountryCodeAnd10Digits_shouldReturnTrue(){
+
+        boolean validationStatus = userValidation.validateContactNumber("91 8862010179");
+        Assert.assertEquals(true,validationStatus);
+    }
+
+    @Test
+    public void whenGivenContactWithoutCountryCodeAndLessThan10Digits_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateContactNumber("882010179");
+        Assert.assertEquals(false,validationStatus);
+    }
+
+    @Test
+    public void whenGivenContactWithCountryCodeAndLessThan10Digits_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateContactNumber(" 91 88620101");
+        Assert.assertEquals(false,validationStatus);
+    }
+    @Test
+    public void whenGivenContactWithoutCountryCodeAndWith10Digits_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateContactNumber("8862010179");
+        Assert.assertEquals(false,validationStatus);
+    }
+    @Test
+    public void whenGivenContactWithAlphabetsAndSpecialCharacters_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateContactNumber("882ed%0179");
+        Assert.assertEquals(false,validationStatus);
+    }
+
 }
