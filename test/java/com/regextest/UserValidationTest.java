a16b9d5b22abaea2920923751252550fa4149eb9
mrunal08 <mrunalg96@gmail.com>
2021-09-08 19:43:03 +0530

    UC5-Password Validation

--- test/java/com/regextest/UserValidationTest.java
+++ test/java/com/regextest/UserValidationTest.java
@@ -4,5 +4,47 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserValidationTest {
-   
+    private UserValidation userValidation = new UserValidation();
+
+    @Test
+    public void whenGivenPasswordWithMinimum8Characters_shouldReturnTrue(){
+
+        boolean validationStatus = userValidation.validateFirstName("Mrunal08");
+        Assert.assertEquals(true,validationStatus);
+    }
+
+    @Test
+    public void whenGivenPasswordWithOutMinimum8Characters_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateFirstName("Mrunal");
+        Assert.assertEquals(false,validationStatus);
+    }
+    @Test
+    public void whenGivenPasswordWithFirstLetterCapital_shouldReturnTrue(){
+
+        boolean validationStatus = userValidation.validateFirstName("Mrunal@08");
+        Assert.assertEquals(true,validationStatus);
+    }
+
+    @Test
+    public void whenGivenPasswordWithFirstLetterNOtCapital_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateFirstName("mrunal@08");
+        Assert.assertEquals(false,validationStatus);
+    }
+
+    @Test
+    public void whenGivenPasswordWithAtLeast1NumericCharacter_shouldReturnTrue(){
+
+        boolean validationStatus = userValidation.validateFirstName("Mrunal@08");
+        Assert.assertEquals(true,validationStatus);
+    }
+
+    @Test
+    public void whenGivenPasswordWithOutAtLeast1NumericCharacter_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateFirstName("Mrunalsg@");
+        Assert.assertEquals(false,validationStatus);
+    }
+
 }
