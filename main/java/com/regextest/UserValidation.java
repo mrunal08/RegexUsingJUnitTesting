190dfa5095f7d8ebab209945c724b42f5c777063
mrunal08 <mrunalg96@gmail.com>
2021-09-09 11:10:05 +0530

    Email Validation for all Samples

--- main/java/com/regextest/UserValidation.java
+++ main/java/com/regextest/UserValidation.java
@@ -1,15 +1,24 @@
 package com.regextest;
 
+import java.io.CharArrayReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidation {
 
-    public boolean validateEmail(String emailSample){
+    public boolean validateEmail(String[] array){
 
-        String emailRegex= "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-        Pattern pattern = Pattern.compile(emailRegex);
-        Matcher matcher = pattern.matcher(emailSample);
-        return matcher.matches();
+
+        boolean result=false;
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<array.length;i++){
+            String emailRegex= "^([a-zA-Z0-9+_.-]+)@([a-zA-Z0-9.-]+){8,15}$";
+            Pattern pattern = Pattern.compile(emailRegex);
+            Matcher matcher = pattern.matcher(array[i]);
+            result= matcher.matches();
+        }
+        return result;
     }
 }
--- test/java/com/regextest/UserValidationTest.java
+++ test/java/com/regextest/UserValidationTest.java
@@ -7,12 +7,24 @@ public class UserValidationTest {
 
     private UserValidation userValidation = new UserValidation();
 
+    String [] array ={"abc-100@yahoo.com","abc@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com",
+            "abc@gmail.com.com","abc+100@gmail.com"};
     @Test
     public void whenGivenEmailMatchingAllSamples_shouldReturnTrue(){
 
-        boolean validationStatus = userValidation.validateEmail("mrunalg96@gmail.com");
+        boolean validationStatus = userValidation.validateEmail(array);
         Assert.assertEquals(true,validationStatus);
     }
 
-   
+    String[] array1={
+            "abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com",
+            "abc()*@gmail.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au",
+    };
+
+    @Test
+    public void whenGivenEmailAreNotMatchingAllSamples_shouldReturnFalse(){
+
+        boolean validationStatus = userValidation.validateEmail(array1);
+        Assert.assertEquals(false,validationStatus);
+    }
 }
