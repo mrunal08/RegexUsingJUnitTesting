4c957421341d10e0922cbe47b54925d5f47dd3b8
mrunal08 <mrunalg96@gmail.com>
2021-09-09 11:21:42 +0530

    UC2 in master

--- main/java/com/regextest/UserValidation.java
+++ main/java/com/regextest/UserValidation.java
@@ -5,16 +5,13 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
     public boolean validateFirstName(String firstName){
-
-<<<<<<< HEAD
-    public boolean validateLastName(String lastName){
-=======
         String uc1Regex= "^[A-Z]{1}[a-zA-Z]{2,20}";
         Pattern pattern = Pattern.compile(uc1Regex);
         Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
     }
->>>>>>> master
+
+    public boolean validateLastName(String lastName){
 
         String UC2Regex= "^[A-Z]{1}[a-zA-Z]{2,20}";
         Pattern pattern = Pattern.compile(UC2Regex);
